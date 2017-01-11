ea4e71e45e3886b61e10d5b11755572838e17686
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-01-11 22:26:29 +0000

    Finished the assignment

--- char-positioning.js
+++ char-positioning.js
@@ -1,16 +1,16 @@
-function countLetters(str){
+function charPosition(str){
   var lower = str.toLowerCase();
   var noSpaces = lower.replace(/ /g,'');
   var myObj = {};
 
   for (var i = 0; i < noSpaces.length; i++) {
     if (myObj[noSpaces[i]] === undefined) {
-      myObj[noSpaces[i]] = 0;
+      myObj[noSpaces[i]] = [];
     }
-    myObj[noSpaces[i]]++;
+    myObj[noSpaces[i]].push(i);
 
   }
   return myObj;
 }
 
-console.log(countLetters('Cars are fast'));
+console.log(charPosition('floor'));
